2448e1ddd4bdc11b89f2f4a5c8ae5c07a58ca3fc
Alex Naupay <alexnaupay@gmail.com>
2024-07-29 00:46:49 -0500

    sdk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,12 +15,12 @@ keystoreProperties.load(keystoreFile.inputStream())
 
 android {
     namespace = "com.alexnaupay.hselfiecamera"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.alexnaupay.hselfiecamera"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
